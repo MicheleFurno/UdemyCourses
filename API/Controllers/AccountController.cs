88bff57ef4c29764905eaf8993a231753eb095c2
Michele Furno <m.furno@fastcode.it>
2024-09-12 11:40:38 +0200

    Authentication added

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO) {
+        using var hmac = new HMACSHA512();
+
+        if(await UserExists(registerDTO.UserName)) return BadRequest("UserName is Taken.");
+
+        var user = new AppUser {
+            UserName = registerDTO.UserName.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+
+        await context.SaveChangesAsync();
+
+        return Ok(new UserDTO {
+            UserName = user.UserName,
+            Token = tokenService.CreateToken(user)
+        });
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO) {
+
+        var user = await context.Users.FirstOrDefaultAsync(user => user.UserName == loginDTO.UserName.ToLower());
+
+        if(user == null) return Unauthorized("Invalid UserName");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for(int i = 0; i < computedHash.Length; i++) {
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+
+        return Ok(new UserDTO {
+            UserName = user.UserName,
+            Token = tokenService.CreateToken(user)
+        });
+    }
+
+    private async Task<bool> UserExists(string userName) {
+        return await context.Users.AnyAsync(user => user.UserName.Trim().ToLower() == userName.Trim().ToLower());
+    }
+
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,18 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
+[Authorize]
+public class UsersController(DataContext context) : BaseApiController
 {
+
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
         var users = await context.Users.ToListAsync();
 
