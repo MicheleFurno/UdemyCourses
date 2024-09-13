2aaccb861b756725345f436125bcae1675ee992f
Michele Furno <m.furno@fastcode.it>
2024-09-13 16:51:41 +0200

    Added member details

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: '', runGuardsAndResolvers: 'always', canActivate: [authGuard], children: [
         {path: 'members', component: MemberListComponent},
-        {path: 'members/:id', component: MemberDetailComponent},
+        {path: 'members/:userName', component: MemberDetailComponent},
         {path: 'lists', component: ListsComponent},
         {path: 'messages', component: MessagesComponent} 
     ]},
